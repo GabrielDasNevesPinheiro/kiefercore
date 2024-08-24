e7c67490483402784732458916377bc6381ac128
gabrieldasnevespinheiro <gabrieldasnevespinheiro@gmail.com>
2024-08-24 13:44:10 -0300

    add activities and intents

--- src/validators/filesystem.ts
+++ src/validators/filesystem.ts
@@ -1,21 +1,20 @@
 import { join } from "path";
 import { existsSync } from "fs";
-import { exit } from "process";
-
 
 // This script checks the filesystem integrity
 
 const paths = [
     join("src", "commands", "Command.ts"),
-    join("src", "configuration", "intents.ts")
+    join("src", "configuration", "intents.ts"),
+    join("src", "configuration", "activities.ts"),
 ]
 
 const checks = paths.map((path) => existsSync(path))
 
 export function checkIntegrity() {
-    const canProceed = checks.find((element) => element == false) as boolean
+    const canProceed = checks.find((element) => element == false) as boolean;
     
-    if(!canProceed) {
+    if(canProceed == false) {
         throw new Error(`Missing necessary file: ${paths[checks.indexOf(canProceed)]}`)
     }
 }
