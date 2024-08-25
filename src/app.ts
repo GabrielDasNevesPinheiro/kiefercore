09c8d96020e559ad05a760431a25615adfe01c47
gabrieldasnevespinheiro <gabrieldasnevespinheiro@gmail.com>
2024-08-25 11:58:21 -0300

    dynamic events feature

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import { Client } from "discord.js";
 import { config } from "dotenv";
-import executeAction from "./handler/InteractionHandler";
 import postSlashCommands from "./api/Register";
 import { checkIntegrity } from "./validators/filesystem";
 import { intents } from "./bot/configuration/intents";
-import { setupActivities } from "./utils/client";
+import { loadEvents } from "./utils/client";
 config();
 
 // checking application structure integrity
@@ -14,19 +13,6 @@ const client = new Client({
     intents
 });
 
-client.on("ready", (client) => {
-    setupActivities(client);
-    console.log("Done my rooster...");
-});
-
-client.on("interactionCreate", async (interaction) => {
-
-    if (!interaction.isChatInputCommand()) return;
-    if (!interaction.channel) return;
-
-    executeAction(interaction.commandName, interaction);
-
-});
-
+loadEvents(client);
 postSlashCommands();
 client.login(process.env.TOKEN);
